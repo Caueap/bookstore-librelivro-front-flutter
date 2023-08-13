d63f946d80bab61064b30434c484969876fb4c22
caue <caueabproenca@gmail.com>
2023-08-13 19:51:33 -0300

    Implemented colored containers to indicate the status of the book

--- librelivro_front_flutter/lib/views/rentals/rental_list_card.dart
+++ librelivro_front_flutter/lib/views/rentals/rental_list_card.dart
@@ -12,8 +12,8 @@ class RentalListCard extends StatelessWidget {
   final String rentalStatus;
   final VoidCallback? handle;
 
-  RentalListCard({
-      required this.rental,
+  RentalListCard(
+      {required this.rental,
       required this.reFetch,
       required this.rentalStatus,
       required this.handle});
@@ -42,29 +42,35 @@ class RentalListCard extends StatelessWidget {
                       Text(
                           'Data esperada de devolução: ${rental.expectedDeliveryDate}'),
                       Text('Data de devolução: ${rental.deliveryDate}'),
-                      Text('Status: ${rental.status}'),
                       Text('Usuário: ${rental.clientModel!.name}'),
                     ],
                   ),
                 )),
           ),
-          Align(
-            alignment: Alignment.centerRight,
+           // Adding space before status
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 2),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                IconButton(
-                  icon: Icon(Icons.check_circle,
-                      color: rentalStatus != 'Pendente'
-                          ? Colors.grey
-                          : Theme.of(context).primaryColor),
-                  onPressed: rentalStatus != 'Pendente'
-                      ? null
-                      : () {
-                          
-                          handle!();
-                        },
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: getStatusColor(rental.status),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      padding:
+                          const EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+                      child: Text(
+                        'Status: ${rental.status}',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
                 ),
+                SizedBox(
+                    width: 8), // Adding space between status and Delete button
                 IconButton(
                     icon: Icon(Icons.delete,
                         color: rentalStatus == 'Pendente'
@@ -74,13 +80,56 @@ class RentalListCard extends StatelessWidget {
                         ? null
                         : () {
                             deleteRental(context);
-                          })
+                          }),
               ],
             ),
-          )
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: GestureDetector(
+                    onTap: rentalStatus != 'Pendente' ? null : handle,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: rentalStatus != 'Pendente'
+                            ? Colors.grey
+                            : Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      padding:
+                          const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+                      child: Center(
+                        child: Text(
+                          'Devolver',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
+  }
+
+  Color getStatusColor(String status) {
+    switch (status) {
+      case 'Entregue no prazo':
+      return Colors.green;
+      case 'Entregue antecipadamente':
+        return Colors.green;
+      case 'Entregue com atraso':
+        return Colors.red;
+      case 'Pendente':
+        return Colors.orange;
+      default:
+        return Colors.grey; // Default color if none of the above matches
+    }
   }
 
   void deleteRental(BuildContext context) async {
